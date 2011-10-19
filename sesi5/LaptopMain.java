e70b01e9738606e844c9cfd7f4dae69e0da605cb
bowo <matrix_saw@ymail.com>
2011-10-19 13:36:21 +0700

    tugas

---
+++ sesi5/LaptopMain.java
@@ -0,0 +1,45 @@
+import.java.io.BufferedInputStream;
+import.java.io.DataInputStream;
+import.java.io.File;
+import.java.io.FileInputStream;
+import.java.io.IOException;
+import.java.util.ArrayList;
+import.java.util.List;
+
+public class LaptopMain {
+     public static void main(String[] args) {
+          File file = new File("F:\SEMESTER 5\PEMPROGRAMAN 1\sesi5\Laptop.txt");
+		  FileInputStream fis = null;
+		  BufferedInputStream bis = null;
+		  DataInputStream dis = null;
+		  List<Laptop> listLaptop = new ArrayList<Laptop>();
+		  
+		  try{
+		      fis = new FileInputStream(file);
+			  bis = new BufferedInputStream(fis);
+			  dis = new DataInputStream(bis);
+			  
+			  while(dis.available() != 0){
+			  //memecah text perbaris menjadi beberapa kolom sesuai dengan pemisah koma
+			  String line = dis.redline();
+			  String[] data = line.split(".");
+			  
+			  Laptop l = new Laptop();
+			  l.setMerk(data[0]);
+			  l.setProcessor(data[1]);
+			  l.setHarga(data[2]);
+			  
+			  listLaptop.add(l);
+			  }
+			  } catch (IOException ex) {
+			       System.out.println("File " + file.getName() + " tidak ketemu !");
+				   System.out.println("Exception : " + ex.getMessage());
+			 }
+			 for (Laptop lp : listLaptop) {
+			 System.out.println("=========================");
+			 System.out.println("Merk : " + lp.getMerk());
+			 System.out.println("Processor : " + lp.getProcessor());
+			 System.out.println("Harga : " + lp.getharga());
+			 }
+	    }
+}
---
+++ sesi6/4/Method.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Method {
+     public static void main(String[] args) {
+          Scanner input = new Scanner(System.in);
+          Tuna tunaObject = new Tuna();
+
+          System.out.println("Enter you name here : ");
+          String name = input.nextLine();
+
+          tunaObject.simpleMessage(name);
+    }
+}
